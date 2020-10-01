7be4398ec11586385c94f3fa2a4e857cd3bff374
asassu <alessio.sassu@bnlpositivity.it>
2020-10-01 16:23:40 +0200

    aggiunta gestione properties

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "starting jar"
-#java -jar /BATCH/bin/batch-transaction-filter.jar --spring.config.location=/BATCH/conf/config.properties
-java -jar /BATCH/bin/batch-transaction-filter.jar 
+# definisce la funzione che  valorizza i placeholder presenti sui file 
+# di configurazione con i valori definiti nelle variabili di ambiente
+set_property_in_file() {
+    FILE="$1"
+    PATTERN="$2"
+    VALUE="$3"
+    
+    sed "s/\#$PATTERN\#/$VALUE/g" $FILE -i
+}
+	     
+# esegue la funzione set_properties_in_file valorizzando il file di configurazione
+set_property_in_file /BATCH/conf/config.properties "EXAMPLE" $EXAMPLE
+
+# avvio eseguibile, con properties adeguate
+java -jar /BATCH/bin/batch-transaction-filter.jar --spring.config.location=/BATCH/conf/config.properties
