2d21c6380e8d098b224de8b61fbcf4c476033199
alessiosassu <alessio.sassu@bnlpositivity.it>
2020-10-07 16:53:15 +0200

    aggiunta lib per connector mysql

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # avvio eseguibile, con properties adeguate
-java -jar /BATCH/bin/batch-transaction-filter.jar --spring.config.location=/BATCH/conf/application.yml
+java -cp "/BATCH/bin/batch-transaction-filter.jar;/BATCH/bin/mysql-connector-java-8.0.21.jar" -Dloader.main=RtdTransactionFilterApplication org.springframework.boot.loader.PropertiesLauncher --spring.config.location=file:/BATCH/conf/
