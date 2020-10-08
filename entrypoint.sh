73162c2b1d67a8dff4dad067e75df5e4d3c307f3
alessiosassu <alessio.sassu@bnlpositivity.it>
2020-10-08 14:21:05 +0200

    modificato caricamento classpath

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # avvio eseguibile, con properties adeguate
-java -cp "/BATCH/bin/batch-transaction-filter.jar;/BATCH/bin/mysql-connector-java-8.0.21.jar" -Dloader.main=RtdTransactionFilterApplication org.springframework.boot.loader.PropertiesLauncher --spring.config.location=file:/BATCH/conf/
+java -cp "/BATCH/bin/batch-transaction-filter.jar:/BATCH/bin/mysql-connector-java-8.0.21.jar" -Dloader.main=it.gov.pagopa.rtd.transaction_filter.RtdTransactionFilterApplication org.springframework.boot.loader.PropertiesLauncher -jar /BATCH/bin/batch-transaction-filter.jar  --spring.config.location=file:/BATCH/conf/
